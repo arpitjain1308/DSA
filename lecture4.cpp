0132557f8f42272885d60b0d91b9309dbcc253a9
ARPIT JAIN <arpitjain1308@gmail.com>
2026-01-21 23:59:28 +0530

    Day 4

--- lecture4.cpp
+++ lecture4.cpp
@@ -127,26 +127,42 @@ using namespace std;
 //     }
 // }
 
-// 17 Jan
+// 17 Jan Restart
 
 int main() {
-    int n=4;
+    int n=12;
     for(int i=0; i<n; ++i) {
-        for(int j=0; j<n-i; ++j) {
+        for(int j=0; j<n-i-1; ++j) {
             cout<<" ";
         }
-
-        for(int j=0; j<=i; ++j) {
-            cout<<j+1;
+        cout<<"*";
+        if(i==0) {
+            cout<<endl;
+            continue;
         }
-
-        for(int j=i; j>0; --j) {
-            cout<<j;
+        for(int j=0; j<2*i-1; ++j) {
+            cout<<" ";
         }
+        cout<<"*";
 
         cout<<endl;
-        
-    }
+   }
+
+   for(int i=0; i<n-1; ++i) {
+        for(int j=0; j<i+1; ++j) {
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i==n-2) {
+            break;
+        }
+        for(int j=0; j<2*(n-1-i)-3; ++j) {
+            cout<<" ";
+        }
+        cout<<"*";
+        cout<<endl;
+
+   }
 
     return 0;
 }
