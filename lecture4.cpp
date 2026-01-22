d2c9062eb841d040764834f3f43efab27019bc88
ARPIT JAIN <arpitjain1308@gmail.com>
2026-01-22 23:23:12 +0530

    Day 5

--- lecture4.cpp
+++ lecture4.cpp
@@ -129,40 +129,72 @@ using namespace std;
 
 // 17 Jan Restart
 
+// int main() {
+//     int n=4;
+//     for(int i=0; i<n; ++i) {
+//         for(int j=0; j<n-i-1; ++j) {
+//             cout<<" ";
+//         }
+//         cout<<"*";
+//         if(i==0) {
+//             cout<<endl;
+//             continue;
+//         }
+//         for(int j=0; j<2*i-1; ++j) {
+//             cout<<" ";
+//         }
+//         cout<<"*";
+
+//         cout<<endl;
+//    }
+
+//    for(int i=0; i<n-1; ++i) {
+//         for(int j=0; j<i+1; ++j) {
+//             cout<<" ";
+//         }
+//         cout<<"*";
+//         if(i==n-2) {
+//             break;
+//         }
+//         for(int j=0; j<2*(n-2-i)-1; ++j) {
+//             cout<<" ";
+//         }
+//         cout<<"*";
+//         cout<<endl;
+
+//    }
+
+//     return 0;
+// }
+
+
+//Butterfly Pattern
 int main() {
-    int n=12;
+    int n=10;
     for(int i=0; i<n; ++i) {
-        for(int j=0; j<n-i-1; ++j) {
-            cout<<" ";
-        }
-        cout<<"*";
-        if(i==0) {
-            cout<<endl;
-            continue;
-        }
-        for(int j=0; j<2*i-1; ++j) {
-            cout<<" ";
-        }
-        cout<<"*";
-
-        cout<<endl;
-   }
-
-   for(int i=0; i<n-1; ++i) {
         for(int j=0; j<i+1; ++j) {
+            cout<<"*";
+        }
+        for(int j=0; j<2*(n-1-i); ++j) {
             cout<<" ";
         }
-        cout<<"*";
-        if(i==n-2) {
-            break;
+        for(int j=0; j<i+1; ++j) {
+            cout<<"*";
         }
-        for(int j=0; j<2*(n-1-i)-3; ++j) {
-            cout<<" ";
-        }
-        cout<<"*";
         cout<<endl;
-
-   }
+    }
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n-i; ++j) {
+            cout<<"*";
+        }
+        for(int j=0; j<2*i; ++j) {
+            cout<<" ";
+        }
+        for(int j=0; j<n-i; ++j) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
