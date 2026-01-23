4440107898f8ac59651060d2e569ab0a3b5a627a
ARPIT JAIN <arpitjain1308@gmail.com>
2026-01-23 12:26:42 +0530

    Day 6

--- lecture6.cpp
+++ lecture6.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 using namespace std;
+
 int decToBinary(int n) {
     int ans=0;
     int pow=1;
     while(n>0) {
-        int rem=n%2;
-        n=n/2;
-        ans=ans+rem*pow;
+        int d = n%2;
+        ans = ans+d*pow;
         pow=pow*10;
+        n=n/2;
     }
-
     return ans;
 }
+
 int binaryToDec(int n) {
     int ans=0;
     int pow=1;
     while(n>0) {
-        int rem=n%10;
-        n=n/10;
-        ans=ans+rem*pow;
-        pow=pow*2;
+        int d = n%10;
+        n/=10;
+        ans+=d*pow;
+        pow*=2;
     }
     return ans;
 }
 int main() {
-    int n=64;
-    cout<<decToBinary(n);
+    int n=1010;
+    cout<<binaryToDec(n);
     return 0;
 }
