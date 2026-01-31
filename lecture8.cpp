59e8601e73d0a5a83848524a24a8f4f83be05f7f
ARPIT JAIN <arpitjain1308@gmail.com>
2026-01-31 14:10:01 +0530

    Day 12

--- lecture8.cpp
+++ lecture8.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 void linearsearch(int arr[], int n) {
     for(int i=0; i<n; ++i) {
         arr[i]=arr[i]*2;
     }
 }
-void rev(int arr[], int n) {
-    int start=0;
-    int end=n-1;
-    while(start<end) {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
+
+void rev(vector<int> &arr) {
+    int s=0;
+    int e=arr.size()-1;
+    while(s<e) {
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
     }
 }
 int main() {
-    int arr[]= {10,20,30,40,50,60};
-    int n=sizeof(arr)/sizeof(int);
-    rev(arr,n);
-    for(int i=0; i<n; ++i) {
+    vector<int> arr = {10,20,30,40,50,60};
+    rev(arr);
+    for(int i=0; i<arr.size(); ++i) {
         cout<<arr[i]<<" ";
     }
     return 0;
