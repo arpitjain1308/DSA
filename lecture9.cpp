18028b3656c97e832ae03baf7b12c8f1128ada88
ARPIT JAIN <arpitjain1308@gmail.com>
2026-02-01 23:54:32 +0530

    Day 12

--- lecture9.cpp
+++ lecture9.cpp
@@ -8,15 +8,9 @@ int main() {
     vec.push_back(30);
     vec.push_back(-40);
     vec.push_back(50);
-    int max= INT8_MIN;
-    for(int i=0; i<vec.size(); ++i) {
-        int sum=0;
-        for(int j=i; j<vec.size(); ++j) {
-            sum+=vec[j];
-        }
-        if(sum>max) {
-            max=sum;
-        }
-    }
-    cout<<max;
+    
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity();
+
+    return 0;
 }
