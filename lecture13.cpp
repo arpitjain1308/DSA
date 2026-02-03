7ec1cb6a7003a92eeec32714db4c1945f36a5883
ARPIT JAIN <arpitjain1308@gmail.com>
2026-02-03 20:01:56 +0530

    Day 15

--- lecture13.cpp
+++ lecture13.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
 int main() {
-    vector<int> vec(10, 0);
+    vector<int> vec = {3,-4,5,4,-1,7,-8};
+    int n=vec.size();
+    int maxs=INT8_MIN;
+    for(int i=0; i<n; ++i) {
+        int curr = 0;
+        for(int j=i; j<n; ++j) {
+            curr+=vec[j];
+            maxs=max(curr, maxs);
+            //cout<<curr<<endl;
+        }
+    }
+
+    cout<<maxs;
+    return 0;
 }
