025ce5083d6042073448f98175fd70b800367533
ARPIT JAIN <arpitjain1308@gmail.com>
2026-02-10 16:48:59 +0530

    Day 21

--- Readme.md
+++ Readme.md
@@ -20,3 +20,4 @@ Day 17 completed.<br>
 Day 18 completed.<br>
 Day 19 completed.<br>
 Day 20 completed.<br>
+Day 21 completed.<br>
---
+++ power_x^n.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int x=5, n=3;
+    cout<<pow(x,n);
+
+    return 0;
+}
