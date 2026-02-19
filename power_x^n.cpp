ae955be10da4b2cc68d27f0084011494a8b64f76
ARPIT JAIN <arpitjain1308@gmail.com>
2026-02-19 15:38:28 +0530

    Day 29

--- power_x^n.cpp
+++ power_x^n.cpp
@@ -2,9 +2,13 @@
 #include <cmath>
 using namespace std;
 int main() {
-    int bin = 3;
-    int x = 7;
-    int ans=1;
+    long bin = 3;
+    double x = 7.0;
+    if(bin<0) {
+        x=1/x;
+        bin=-bin;
+    }
+    double ans=1;
     while(bin>0) {
         if(bin%2==1) {
             ans*=x;
